458ec550da0805bfcd589041610bf5de230d88ff
de12eoliefant <mitredlum@gmail.com>
2020-01-28 12:28:59 +0100

    ohja nu doet ie het

--- src/game/HumanPlayer.java
+++ src/game/HumanPlayer.java
@@ -17,8 +17,11 @@ public class HumanPlayer extends Player {
      * @param input coordinate
      * @return correctly formatted coordinate
      */
-    public String makeGoodFormat(String input) {
+    public String makeGoodFormat(Board board, String input) {
         String[] marblesplit = input.split(",");
+        while (marblesplit.length != 2 || marblesplit[0].equals("") || marblesplit[1].equals("")) {
+        	input = makeGoodInput(board, input);
+        }
         try {
             Integer.parseInt(marblesplit[0]);
         } catch (NumberFormatException e) {
@@ -41,21 +44,45 @@ public class HumanPlayer extends Player {
         while (marblesplit.length != 2 || marblesplit[0].equals("") || marblesplit[1].equals("")) {
             System.out.println(
                     "> Please try again.\n  " + "Format: CHARACTER,INTEGER or INTEGER,CHARACTER\n  e.g. A,1 or 1,A");
-            result = makeGoodFormat(scanner.nextLine().toUpperCase());
+            result = scanner.nextLine().toUpperCase();
             marblesplit = result.split(",");
         }
+        result = makeGoodFormat(board, result).toUpperCase();
+        boolean hasHorizontal = false;
+       	while (true) {
+       		marblesplit = result.split(",");
+    		for (int i = 0; i < board.horizontal.length; i++) {
+    			if (marblesplit[1].charAt(0) == board.horizontal[i]) {
+    				hasHorizontal = true;
+    			}
+    		}
+    		if (!hasHorizontal) {
+    			 System.out.println(">Invalid input, please try again.\n  "
+                         + "Format: CHARACTER,INTEGER or INTEGER,CHARACTER\n  e.g. A,1 or 1,A");
+                 result = makeGoodInput(board, scanner.nextLine());
+    		}
+    		else {
+    			break;
+    		}
+       	}
         while (true) {
             try {
-                while (board.getMarble(marblesplit[1].charAt(0), Integer.parseInt(marblesplit[0])) != getMarble()) {
-                    System.out.println("> This is not one of your marbles, please try again");
-                    result = makeGoodFormat(scanner.nextLine().toUpperCase());
+            	Marble marble;
+                while ((marble = board.getMarble(marblesplit[1].charAt(0), Integer.parseInt(marblesplit[0]))) != getMarble()) {
+                    if (board.getPlayers() == 4) {
+                    	if (marble == getMarble().next(4).next(4)) {
+                    		break;
+                    	}
+                    }
+                	System.out.println("> This is not one of your marbles, please try again");
+                    result = makeGoodInput(board, scanner.nextLine().toUpperCase());
                     marblesplit = result.split(",");
                 }
                 break;
             } catch (Exception e) {
                 System.out.println("> Invalid input, please try again.\n  "
                         + "Format: CHARACTER,INTEGER or INTEGER,CHARACTER\n  e.g. A,1 or 1,A");
-                result = makeGoodFormat(scanner.nextLine());
+                result = makeGoodInput(board, scanner.nextLine());
                 marblesplit = result.split(",");
             }
         }
@@ -68,10 +95,10 @@ public class HumanPlayer extends Player {
     public String determineMove(Board board) {
         System.out.println("> " + getName() + " (" + getMarble().toString() + "),\n  "
                 + "what is (one of) the outer marble(s) you want to move?\n  e.g. (A,1 or 1,A) ");
-        final String marble1 = makeGoodInput(board, makeGoodFormat(scanner.nextLine().toUpperCase()));
+        final String marble1 = makeGoodInput(board, scanner.nextLine().toUpperCase());
         System.out.println("> What is the other outer marble you want to move?\n  "
                 + "If you only want to move 1 marble, enter the same one again");
-        final String marble2 = makeGoodInput(board, makeGoodFormat(scanner.nextLine().toUpperCase()));
+        final String marble2 = makeGoodInput(board, scanner.nextLine().toUpperCase());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 6; i++) {
             sb.append("\n" + i + ": " + Direction.values()[i]);
